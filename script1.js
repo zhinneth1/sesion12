492e042499fce508d19efca1e6cbe578fff5c77b
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-14 20:00:48 -0500

    ccambiossubir

--- script1.js
+++ script1.js
@@ -4,6 +4,7 @@ let promedio=0;
 for(let i=0; i < numero.length; i++){
     suma=+numero[i];
 }
-promedio/numero.length;
-document.getElementById("Resultado").value=suma;
-document.getElementById("Promedio").value=promedio;
+
+promedio = suma/numero.length;
+document.getElementById(resultado).value=suma;
+document.getElementById(promedio).value=suma;
