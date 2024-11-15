326d0ee0f7fb02ee025eb36abd67762b42db60c1
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-14 20:08:54 -0500

    s

--- script1.js
+++ script1.js
@@ -2,9 +2,9 @@ let numero = [1,2,3,4,5];
 let suma=0;
 let promedio=0;
 for(let i=0; i < numero.length; i++){
-    suma=+numero[i];
+    suma+=numero[i];
 }
 
 promedio = suma/numero.length;
-document.getElementById("resultado".value=suma);
-document.getElementById("promedio".value=promedio);
+document.getElementById("resultado").value=suma;
+document.getElementById("promedio").value=promedio;
